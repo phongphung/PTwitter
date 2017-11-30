1e3d5e686401dcc813170728c71690e14a44b836
PhongPhung <phongifls@gmail.com>
2017-11-30 13:13:24 +0700

    add limit for get friends and followers

--- Main.py
+++ Main.py
@@ -2,11 +2,12 @@ from TwitterAuth.AuthProvider import PTweepy
 
 if __name__ == '__main__':
     api = PTweepy()
-    screen_names = ['scifigene','gradiz_lenin','asmaabouderka','Isamarella']
-    ids = ['1000004156', '1000010568', '100000801']
+    screen_names = ['asmaabouderka','Isamarella']
+    ids = ['1000004156', '1000010568']
     temp1 = api.get_user_info_by_ids(ids)
     temp2 = api.get_user_info_by_screen_name(screen_names)
-    temp3 = api.get_followers_by_screen_name(screen_names)
-    temp4 = api.get_followers_by_id(ids)
-    temp5 = api.get_friends_by_id(ids)
-    temp6 = api.get_friends_by_screen_name(screen_names)
+    temp3 = api.get_followers_by_screen_name(screen_names, info=True, limit=10000)
+    temp4 = api.get_followers_by_id(ids, info=True, limit=5000)
+    temp5 = api.get_friends_by_id(ids, info=True, limit=10000)
+    temp6 = api.get_friends_by_screen_name(screen_names, info=True, limit=10000)
+    print('{}, {}, {}, {}, {}, {}'.format(temp1, temp2, temp3, temp4, temp5, temp6))
